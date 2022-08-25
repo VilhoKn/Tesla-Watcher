ead85884d291cbe0442c875addcb25d02c811e86
unknown <knuutila.vilho@gmail.com>
2022-08-25 22:22:38 +0300

    oops forgot to commit xd

---
+++ src/code/index.js
@@ -0,0 +1,54 @@
+const teslaInventory = require('tesla-inventory')
+const path = require('path')
+const axios = require('axios')
+const fs = require("fs")
+
+const models = ["s"]
+const countries = ["de", "fi"]
+
+const countryCodeMap = {
+	"de": "Saksa",
+	"fi": "Suomi"
+}
+
+function checkTesla(model, country) {
+	const promise = teslaInventory(country, {
+		model: model,
+		condition: 'used'
+	})
+
+	return promise
+}
+
+function notify(model, country) {
+	previousCars = JSON.parse(fs.readFileSync(path.join(__dirname, '../files/previousCars.json')).toString())
+	fs.writeFileSync(path.join(__dirname, '../files/previousCars.json'), JSON.stringify([...country, model]), (err) => {if (err) throw err;})
+
+	axios.post("https://monkeman.pythonanywhere.com/api/tesla", {model, country})
+}
+
+function checkTeslas() {
+
+	previousCars = JSON.parse(fs.readFileSync(path.join(__dirname, '../files/previousCars.json')).toString())
+
+	for(i of models) {
+		for(j of countries) {
+			const promise = checkTesla(i, j)
+			promise.then(results => {
+				if(results.length === 0) return
+				for(k of results) {
+					if (!previousCars.includes(k.VIN)) {
+						notify(i.toUpperCase(), countryCodeMap[k.CountryCode.toLowerCase()])
+					}
+				}
+			} )
+		}
+	}
+}
+
+checkTeslas()
+
+
+
+
+
